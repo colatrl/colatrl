28a0efce20171538f16f8160e3f809aaf3d9e58d
Josh Bailey <josh@vandervecken.com>
2025-11-04 16:00:34 +1300

    libc.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,6 @@ set -e
 
 BUILD_SH="$(readlink -f "${BASH_SOURCE[0]:-$0}")"
 BUILD_DIR="$(dirname "$BUILD_SH")/../src"
-sudo apt-get -yq update && sudo apt-get -yq install clang-19 g++14
+sudo apt-get -yq update && sudo apt-get -yq install clang-19 g++14 libc6-dev-i386
 cd $BUILD_DIR
 make clean && make
